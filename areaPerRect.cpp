aa99096779023b939a1f2248cd968178d04c6b91
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-19 04:38:57 +0000

    updatedAreaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright 2025 Xiaohan Tian. All rights reserved.
+// Created by Xiaohan Tian
+// Created on June 2020
+// This program calculates the area and perimeter of a rectangle.
+//    with dimensions of 7cm x 9cm
+
+#include<iostream>
+
+int main() {
+    std::cout <<"If a rectangle has the dimensions:" << std::endl;
+    std::cout << "7cm x 9cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << (7 * 9) << "cm^2" << std::endl;
+    std::cout << "Perimeter is:" << (2* (7+9)) << "cm" << std::endl;
+}
